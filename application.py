1fbbbdc2a930ae252b259dfb7d62ae588849b8e7
Ahmed Morsi <amorsi@berkeley.edu>
2025-07-21 13:13:18 +0300

    refactored code for cloud deployment and create CPU-only docker container

--- application.py
+++
@@ -1,7 +0,0 @@
-from src.serve_model import create_prediction_app
-
-# Create app instance for production
-app = create_prediction_app(initialize_models_on_startup=True)
-
-if __name__ == "__main__":
-    app.run(debug=False)
